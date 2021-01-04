f47dce670382a560d1692e1b845823af67a08e16
agusfesc <agusfranesc@gmail.com>
2021-01-04 11:39:26 -0300

    improvements in code of competitions tabs

--- components/Section/index.js
+++ components/Section/index.js
@@ -2,18 +2,16 @@ import React from 'react'
 
 
 // eslint-disable-next-line react/prop-types
-const Section = ({title, handleOpen, handleClose, handleCloseTwo, isActive,}) => {
+const Section = ({title, handleOpen, value, isActive,}) => {
   
-	const buttonActive = 'px-3 border-b-2 border-blue-500 transition delay-100 duration-300 text-white text-xs md:text-base break-words pb-1.5 w-4/12 md:w-1/5 text-center cursor-pointer '
-	const buttonDisable = 'px-3 border-b-2 border-transparent transition delay-100 duration-300 text-white text-xs md:text-base break-words pb-1.5 w-4/12 md:w-1/5 text-center cursor-pointer'
+	const buttonActive = 'px-3 border-b-2 border-blue-500 transition delay-100 duration-300 text-white text-xxs md:text-base break-words pb-1.5 w-3/12 md:w-1/5 text-center cursor-pointer '
+	const buttonDisable = 'px-3 border-b-2 border-transparent transition delay-100 duration-300 text-white text-xxs md:text-base break-words pb-1.5 w-3/12 md:w-1/5 text-center cursor-pointer'
   
 	return ( 
 		<a 
 			className={isActive ? buttonActive : buttonDisable}
 			onClick={ () => {
-				handleOpen(true)
-				handleClose(false)
-				handleCloseTwo(false)
+				handleOpen(value)
 			}} 
 		>
 			{title}
