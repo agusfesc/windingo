dc3b9101e511d9d6750aaa281167541ba0f9c818
agusfesc <agusfranesc@gmail.com>
2021-01-13 14:54:48 -0300

    run eslint

--- services/RequestServices/index.js
+++ services/RequestServices/index.js
@@ -1,16 +1,16 @@
 const baseURL = process.env.NEXT_PUBLIC_FACEIT_URL
 
 function RequestService ({ param }) {
-  var myHeaders = new Headers()
-  myHeaders.append("Authorization", `Bearer ${process.env.NEXT_PUBLIC_FACEIT_KEY}`)
+	var myHeaders = new Headers()
+	myHeaders.append('Authorization', `Bearer ${process.env.NEXT_PUBLIC_FACEIT_KEY}`)
 
-  var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
+	var requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow'
+	}
 
-  return fetch(`${baseURL}${param}`, requestOptions)
+	return fetch(`${baseURL}${param}`, requestOptions)
   
 }
 
